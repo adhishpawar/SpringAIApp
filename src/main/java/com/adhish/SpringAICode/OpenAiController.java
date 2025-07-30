957d0c46af031bf27e18246c83ca89b9959caf10
adhish <adhishpawar@gmail.com>
2025-07-30 10:39:37 +0530

    Memory Advisor Added

--- src/main/java/com/adhish/SpringAICode/OpenAiController.java
+++ src/main/java/com/adhish/SpringAICode/OpenAiController.java
@@ -1,18 +1,50 @@
 package com.adhish.SpringAICode;
 
-import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
-import org.springframework.ai.chat.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class OpenAiController {
 
-    @Autowired
-    private VertexAiGeminiChatModel chatModel;
+    private final ChatClient chatClient;
 
-    @GetMapping("/api/{message}")
-    public String getResponse(@PathVariable String message) {
-        return chatModel.call(message);
+    //For more than one models are in use
+//   public OpenAiController(OpenAiChatModel chatModel){
+//       this.chatClient = ChatClient.create(chatModel);
+//   }
+
+    ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+    //For single model in use
+    public OpenAiController(ChatClient.Builder builder){
+        this.chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor
+                        .builder(chatMemory)
+                        .build())
+                .build();
+    }
+
+    @PostMapping("/api/{message}")
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+        ChatResponse chatResponse = chatClient
+                .prompt(message)
+                .call()
+                .chatResponse();
+
+        System.out.println(chatResponse.getMetadata().getModel());
+
+
+        String response = chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return  ResponseEntity.ok(response);
     }
 }

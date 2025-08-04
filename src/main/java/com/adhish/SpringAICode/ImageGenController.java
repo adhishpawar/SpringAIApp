c4523288b87889a30244d0d3adacff9d5b9dedff
adhish <adhishpawar@gmail.com>
2025-08-04 21:43:31 +0530

    OpenAI Image Model

---
+++ src/main/java/com/adhish/SpringAICode/ImageGenController.java
@@ -0,0 +1,35 @@
+package com.adhish.SpringAICode;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.image.ImageProducer;
+
+@RestController
+public class ImageGenController {
+
+    private ChatClient chatClient;
+
+    private OpenAiImageModel OpenAiImageModel;
+
+    public ImageGenController(OpenAiImageModel OpenAiImageModel, OpenAiChatModel chatModel){
+        this.OpenAiImageModel= OpenAiImageModel;
+        this.chatClient = ChatClient.create(chatModel);
+    }
+
+    @GetMapping("image/{query}")
+    public String genImage(@PathVariable String query){
+
+        ImagePrompt prompt = new ImagePrompt(query);
+        ImageResponse response = OpenAiImageModel.call(prompt);
+        return response.getResult().getOutput().getUrl();
+    }
+}

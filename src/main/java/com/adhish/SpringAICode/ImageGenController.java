44fe471cf9e8faf4029165675ed0aa704d18ba25
adhish <adhishpawar@gmail.com>
2025-08-07 11:53:08 +0530

    Impl describe Image

--- src/main/java/com/adhish/SpringAICode/ImageGenController.java
+++ src/main/java/com/adhish/SpringAICode/ImageGenController.java
@@ -6,9 +6,10 @@ import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ImageGenController {
@@ -34,5 +35,16 @@ public class ImageGenController {
 
         ImageResponse response = OpenAiImageModel.call(prompt);
         return response.getResult().getOutput().getUrl();
+    }
+
+    @PostMapping("image/describe")
+    public String descImage(@RequestParam String query, @RequestParam MultipartFile file){
+        // User uses consumer object UserSpec
+        return chatClient.prompt()
+                .user(us -> us.text(query)
+                        .media(MimeTypeUtils.IMAGE_JPEG, file.getResource()))
+                .call()
+                .content();
+
     }
 }

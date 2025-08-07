6f4d716a63bcb582c58304055ac6d120c044bf4c
adhish <adhishpawar@gmail.com>
2025-08-07 13:23:25 +0530

    Bean o/p Converter,with List

--- src/main/java/com/adhish/SpringAICode/MovieController.java
+++ src/main/java/com/adhish/SpringAICode/MovieController.java
@@ -4,8 +4,10 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,6 +49,60 @@ public class MovieController {
         List<String> movies = opCon.convert(chatClient.prompt(prompt).call().content());
 
         return movies;
+    }
+
+    @GetMapping("/movie")
+    public Movie getMovieData(@RequestParam String name){
+
+        String message = """
+                                  Get me the best Movie of {name}
+                                   {format}
+                         """;
+
+
+        BeanOutputConverter<Movie> opCon = new BeanOutputConverter<>(Movie.class);
+
+        PromptTemplate template = PromptTemplate.builder()
+                .template(message)
+                .variables(Map.of(
+                        "name", name,
+                        "format", opCon.getFormat()
+                ))
+                .build();
+
+        Prompt prompt = template.create();
+        Movie movie = opCon.convert(chatClient.prompt(prompt).call().content());
+
+        return movie;
+
+    }
+
+    @GetMapping("/moviesList")
+    public List<Movie> getMovieList(@RequestParam String name){
+
+        String message = """
+                                 Top 5 Movie of {name}
+                                  {format}
+                         """;
+
+
+        BeanOutputConverter<List<Movie>> opCon = new BeanOutputConverter<>(
+                new ParameterizedTypeReference<List<Movie>>() {}
+        );
+
+        PromptTemplate template = PromptTemplate.builder()
+                .template(message)
+                .variables(Map.of(
+                        "name", name,
+                        "format", opCon.getFormat()
+                ))
+                .build();
+
+        Prompt prompt = template.create();
+        List<Movie> movies = opCon.convert(chatClient.prompt(prompt).call().content());
+
+        return movies;
+
     }
 
 }
